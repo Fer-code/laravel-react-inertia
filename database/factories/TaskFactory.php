b7f9714185351cfe9dd80ee131978b30d8ad22e2
Fernanda Rocha Miranda <fernandamirandacursos@gmail.com>
2025-03-28 10:42:20 -0300

    correcting task factory

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -19,7 +19,7 @@ class TaskFactory extends Factory
         return [
             'name' => fake()->sentence(),
             'description' => fake()->realText(),  
-            'due_date' => fake()->dateTimeBetween('now', '+1 year'),  
+            'due_data' => fake()->dateTimeBetween('now', '+1 year'),  
             'status' => fake()->randomElement(['pending', 'in_progress', 'completed']),  
             'priority' =>  fake()->randomElement(['low', 'medium', 'high']),
             'image_path' => fake()->imageUrl(),  
